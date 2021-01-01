3891bab055c2911a1208dc8c91f052ff1a08ff89
Venkateswararao Yeluru <venkateswararao.yeluru@colortokens.com>
2021-01-01 11:18:35 +0530

    commit

---
+++ src/main/scala/joins/SimpleSparkExample.scala
@@ -0,0 +1,31 @@
+package joins
+
+import org.apache.spark.sql.SparkSession
+
+object SimpleSparkExample extends App {
+
+  val spark = SparkSession.builder().master("local[1]").appName("JoinsExamples").getOrCreate()
+
+  // people table
+  val people = Seq(("venkat", "SE"), ("Alekhya", "VLSI"))
+  val col = Seq("name", "job")
+
+  import spark.sqlContext.implicits._
+
+  val peopleDF = people.toDF(col: _*)
+  peopleDF.show(false)
+
+  //location table
+  val location = Seq(("SE", "Ban"), ("VLSI", "Hyd"))
+  val lCol = Seq("job", "location")
+
+  import spark.sqlContext.implicits._
+
+  val locationDF = location.toDF(lCol: _*)
+  locationDF.show(false)
+
+  //inner join
+  peopleDF.join(locationDF, peopleDF("job") === locationDF("job"), "").show(false)
+
+
+}
